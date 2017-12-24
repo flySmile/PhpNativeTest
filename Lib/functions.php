239113f8f171bd9374e9941d527c220bb41d7c09
FlySmile <870467813@qq.com>
2017-12-25 01:10:51 +0800

    秒滴发送短信

--- Lib/functions.php
+++ Lib/functions.php
@@ -6,3 +6,30 @@
   {
     echo json_encode($var);die;
   }
+
+  /**
+  * post请求
+  */
+  function curlPost($url, $requestString, $headers = []){
+    if($url === '' || $requestString === ''){
+      return false;
+    }
+
+    $con = curl_init((string)$url);
+
+    if (!empty($headers)) {
+      curl_setopt($con, CURLOPT_HEADER, true);
+      curl_setopt($con, CURLOPT_HTTPHEADER, $headers);
+    } else {
+      curl_setopt($con, CURLOPT_HEADER, false);
+    }
+
+    curl_setopt($con, CURLOPT_POST,true);
+    curl_setopt($con, CURLOPT_POSTFIELDS, $requestString);
+    curl_setopt($con, CURLOPT_RETURNTRANSFER,true);
+
+    $result = curl_exec($con);
+    curl_close($con);
+
+    return $result;
+  }
--- Sms/miaodi.php
+++ Sms/miaodi.php
@@ -1,4 +1,25 @@
 <?php
   require dirname(__FILE__) . '/../base.php';
 
-  je($config);
+  $query_url = 'https://api.miaodiyun.com/20150822/industrySMS/sendSMS';
+  $params_miaodi = $config['params']['miaodi'];
+  $verify_base = range(1000, 9999);
+  $verify_num = $verify_base[array_rand($verify_base)];
+  $minute = 5;
+  $timestamp = date('YmdHis');
+  $post_data = [
+    'accountSid' => $params_miaodi['account_sid'],
+    'smsContent' => '【志斌科技】您的验证码为'. $verify_num . '，请于' . $minute . '分钟内正确输入，如非本人操作，请忽略此短信。',
+    'to' => $params_miaodi['phone'],
+    'timestamp' => $timestamp,
+    'sig' => md5($params_miaodi['account_sid'] .  $params_miaodi['auth_token'] . $timestamp),
+    'respDataType' => 'json',
+  ];
+  $headers = [
+    'Content-type:application/x-www-form-urlencoded',
+    'Accept:application/json',
+  ];
+
+  $server_result = curlPost($query_url, http_build_query($post_data), $headers);
+
+  je($server_result);
