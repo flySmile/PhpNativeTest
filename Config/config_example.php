ff63935b4dec319a79b2d9f6a836618fb9106a4e
yuzhibin <yuzhibin@mioji.com>
2017-12-27 17:26:10 +0800

    config

--- Config/config_example.php
+++ Config/config_example.php
@@ -1,25 +1,26 @@
+<?php
 return [
-  //基础参数
-  'params' => [
+    //基础参数
+    'params' => [
 
-  ],
-
-  //MySQL数据库配置
-  'connections' => [
-    'mysql' => [
-      'host' => '127.0.0.1',
-      'username' => 'root',
-      'password' => '11111',
     ],
-  ],
 
-  //Redis配置
-  'redis' => [
-    'default' => [
-          'host' => '127.0.0.1',
-          'password' => null,
-          'port' => 6379,
-          'database' => 0,
+    //MySQL数据库配置
+    'connections' => [
+        'mysql' => [
+            'host' => '127.0.0.1',
+            'username' => 'root',
+            'password' => '11111',
+        ],
     ],
-  ],
+
+    //Redis配置
+    'redis' => [
+        'default' => [
+            'host' => '127.0.0.1',
+            'password' => null,
+            'port' => 6379,
+            'database' => 0,
+        ],
+    ],
 ];
